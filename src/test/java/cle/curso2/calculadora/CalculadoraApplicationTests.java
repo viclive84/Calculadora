eefde36e41aa16765ad7621080d8453a7a2930ae
root <root@localhost.localdomain>
2019-04-11 18:35:44 +0200

    mensaje

---
+++ src/test/java/cle/curso2/calculadora/CalculadoraApplicationTests.java
@@ -0,0 +1,16 @@
+package cle.curso2.calculadora;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculadoraApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
